9f406f73f60891ede2626e217b15b73d6f8cf407
Ishan Manandhar <ishan02016@gmail.com>
2021-03-28 15:56:56 +0545

    added mobile menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log('Scripts are working!');
+window.onload = (_e) => {
+  const openMenu = document.getElementById('open-menu');
+  const closeMenu = document.getElementById('close-menu');
+  const overlayDiv = document.querySelector('.overlaymenu');
+  openMenu.onclick = function () {
+    overlayDiv.classList.add('show');
+  };
+  closeMenu.onclick = function () {
+    overlayDiv.classList.remove('show');
+  };
+};
